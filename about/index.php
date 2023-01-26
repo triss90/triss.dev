ef5ecb3ac818d1b204ed3bd753cb1c5bb12a1251
Tristan White <oliver.tristan@gmail.com>
2023-01-26 13:50:21 +0000

    Update about

--- about/index.php
+++ about/index.php
@@ -13,11 +13,14 @@
 
     <main id="main">
         <div class="central space center">
-            <p>Tristan is a Front-End UI/UX Developer based in Aarhus, Denmark.</p>
-            <p>Tristan works for small companies and startups, creating responsive web applications, and online experiences.</p>
+            <p>Tristan is a Systems Engineer with flair for Front-End UI/UX based in Aarhus Denmark</p>
+            <p>Tristan co-owns and works for ServicePoint A/S, with a primary focus on developing and mainting cloud infrastructure.</p>
             <p>Building the right foundation to ongoing optimization and support. In short, he tries to make the web a better place.</p>
             <p>Tristan has worked with corporations, institutions and startups in a wide range of industries.</p>
-            <p>See small bits of his experimental work on <a href="https://codepen.io/triss90/" target="_blank" rel="noopener">Codepen</a>, projects on <a href="https://github.com/triss90" target="_blank" rel="noopener">GitHub</a>, thoughts on <a href="https://twitter.com/triss90" target="_blank" rel="noopener">Twitter</a>, and video tutorials on <a href="https://www.youtube.com/channel/UCxnhnHu17q4Zk4m9-7BL5dw" target="_blank" rel="noopener">YouTube</a>.</p>
+            <p>See small bits of his experimental work on 
+            <a href="https://codepen.io/triss90/" target="_blank" rel="noopener">Codepen</a>, projects on 
+            <a href="https://github.com/triss90" target="_blank" rel="noopener">GitHub</a>, thoughts on his
+            <a href="/blog">Blog</a> and some of his work <a href="/#work">here</a>.</p>
         </div>
     </main>
 
