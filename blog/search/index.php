a719413ad424ea6c7db12a1fa9855c5b3a7a4923
Tristan White <oliver.tristan@gmail.com>
2023-02-21 14:08:37 +0000

    clean up comments

--- blog/search/index.php
+++ blog/search/index.php
@@ -22,10 +22,9 @@
 
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-        xobj.open('GET', '../post_feed.json', true); // Replace 'my_data' with the path to your file
+        xobj.open('GET', '../post_feed.json', true);
         xobj.onreadystatechange = function () {
             if (xobj.readyState == 4 && xobj.status == "200") {
-                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
                 callback(xobj.responseText);
             }
         };
