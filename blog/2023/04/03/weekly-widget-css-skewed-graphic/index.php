e5ac52c814be2fa5ca2cfa301155b9e2f5099d99
Tristan White <oliver.tristan@gmail.com>
2023-04-03 08:13:34 +0200

    Publish blogpost

---
+++ blog/2023/04/03/weekly-widget-css-skewed-graphic/index.php
@@ -0,0 +1,22 @@
+
+            <?php $description = 'Weekly Widget - CSS Skewed Graphic';  ?>
+            <?php include('../../../../../_inc/header.php'); ?>
+            <?php include('../../../../../_inc/icons.php'); ?>
+            
+            <span class='inverted'><?php include('../../../../../_inc/navigation.php'); ?></span>
+            <main id='main' class='central space post'>
+        <h1>Weekly Widget - CSS Skewed Graphic</h1>
+<p class='timestamp'><time datetime='03-04-2023'>03-04-2023</time></p>
+<hr>
+<p>This week's widget is a simple skewed graphic, made with pure CSS, enjoy:</p>
+<p class="codepen" data-height="807.54296875" data-default-tab="result" data-slug-hash="PoYyNrO" data-user="triss90" style="height: 700px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+  <span>See the Pen <a href="https://codepen.io/triss90/pen/PoYyNrO">
+  CSS graphic</a> by Tristan  White (<a href="https://codepen.io/triss90">@triss90</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+            </main>
+            <script src='../../../../../assets/js/highlight.pack.js'></script>
+            <script>hljs.initHighlightingOnLoad();</script>
+            <?php include('../../../../../_inc/footer.php'); ?>
+        
