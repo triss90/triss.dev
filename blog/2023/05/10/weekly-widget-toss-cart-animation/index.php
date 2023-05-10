d362f5c9418a4e39f2b9e22f21a38f9ac37b5b08
Tristan White <oliver.tristan@gmail.com>
2023-05-10 08:51:40 +0200

    publish weekly widget post

---
+++ blog/2023/05/10/weekly-widget-toss-cart-animation/index.php
@@ -0,0 +1,22 @@
+
+            <?php $description = 'Weekly Widget - "Toss" Add to Cart Animation';  ?>
+            <?php include('../../../../../_inc/header.php'); ?>
+            <?php include('../../../../../_inc/icons.php'); ?>
+            
+            <span class='inverted'><?php include('../../../../../_inc/navigation.php'); ?></span>
+            <main id='main' class='central space post'>
+        <h1>"Toss" Add to Cart Animation</h1>
+<p class='timestamp'><time datetime='10-05-2023'>10-05-2023</time></p>
+<hr>
+<p>This week's widget is a fancy add to cart animation, made with CSS and a bit of JS to make it interactable, enjoy:</p>
+<p class="codepen" data-height="800" data-default-tab="result" data-slug-hash="vYEPxLY" data-user="triss90" style="height: 700px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+  <span>See the Pen <a href="https://codepen.io/triss90/pen/vYEPxLY">
+  "Toss" Add to Cart Animation</a> by Tristan  White (<a href="https://codepen.io/triss90">@triss90</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+            </main>
+            <script src='../../../../../assets/js/highlight.pack.js'></script>
+            <script>hljs.initHighlightingOnLoad();</script>
+            <?php include('../../../../../_inc/footer.php'); ?>
+        
