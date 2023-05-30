0aecdbeddb715e36ea5e8a8ed49521b86a64bac0
Tristan White <oliver.tristan@gmail.com>
2023-05-30 19:02:53 +0200

    Publish blogpost

---
+++ blog/2023/05/30/colordrop-release-2023/index.php
@@ -0,0 +1,34 @@
+
+            <?php $description = 'ColorDrop Release 2023';  ?>
+            <?php include('../../../../../_inc/header.php'); ?>
+            <?php include('../../../../../_inc/icons.php'); ?>
+            
+            <span class='inverted'><?php include('../../../../../_inc/navigation.php'); ?></span>
+            <main id='main' class='central space post'>
+        <h1>ColorDrop Release 2023</h1>
+<p class='timestamp'><time datetime='30-05-2023'>30-05-2023</time></p>
+<hr>
+<p>I am are thrilled to announce the launch of the all-new, redesigned <a href="https://colordop.io">ColorDrop.io</a>!</p>
+<p>Over the past few months, I've been working tirelessly to enhance the ColorDrop experience, and I am now excited to present to you a stunningly refreshed interface, improved image scanning, an upgraded palette customization feature, and a vast library of new color palettes.</p>
+<h2>Introducing the New Interface</h2>
+<p>I have completely reimagined the ColorDrop interface, making it even more intuitive and user-friendly. It's now simpler than ever to navigate the site and create the perfect palette for your design projects.</p>
+<img alt="Image of the ColorDrop website" src="https://triss.dev/examples/colordrop.png" style="max-width: 100%; border: 2px solid #19191a; border-radius: 0.3rem;">
+<h2>Improved Image Scanning</h2>
+<p>The image scanning feature has undergone an extensive upgrade. With the improved algorithm, you can now extract colors from your images with even greater accuracy, enhancing your creativity and design process.</p>
+<img alt="Image of the ColorDrop website" src="https://triss.dev/examples/colordrop1.png" style="max-width: 100%; border: 2px solid #19191a; border-radius: 0.3rem;">
+<h2>Enhanced Palette Customization</h2>
+<p>I've listened to your feedback, and the palette customization feature has been revamped to give you even more control over your color combinations. Experimenting and tweaking until you find the ideal match for your project is now more enjoyable and efficient than ever before.</p>
+<img alt="Image of the ColorDrop website" src="https://triss.dev/examples/colordrop2.png" style="max-width: 100%; border: 2px solid #19191a; border-radius: 0.3rem;">
+<h2>Fresh New Palettes</h2>
+<p>To fuel your inspiration, I've added a plethora of new color palettes. From bold and bright, to soft and soothing, there's something for every design aesthetic.</p>
+<h2>More Frequent Updates</h2>
+<p>I'm committed to continually enriching your ColorDrop experience. Going forward, I will be adding new color palettes much more frequently, ensuring that you always have fresh and current color combinations to inspire your projects.</p>
+<p>I believe that design is an ever-evolving journey, and I'm' here to provide you with the tools you need to express your creativity in the most vibrant ways possible. I am incredibly proud of the new ColorDrop, and can't wait for you to experience it.</p>
+<p>Start exploring the new <a href="https://colordop.io">ColorDrop.io</a> now, and let your color journey begin. I'm excited to see the incredible designs you create!</p>
+<p>Thank you for your continued support and happy coloring!</p>
+<p>/Tristan White</p>
+            </main>
+            <script src='../../../../../assets/js/highlight.pack.js'></script>
+            <script>hljs.initHighlightingOnLoad();</script>
+            <?php include('../../../../../_inc/footer.php'); ?>
+        
