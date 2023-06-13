5634b8944e1ad87a6cc1336b9289a9085defc3e8
Tristan White <oliver.tristan@gmail.com>
2023-06-13 18:51:02 +0200

    Add snippets section

--- _inc/navigation.php
+++ _inc/navigation.php
@@ -3,6 +3,7 @@
         <li><a href="/">Tristan White <span class="vh">Home</span></a></li>
         <li><a href="/#work">Projects</a></li>
         <li><a href="/blog">Articles</a></li>
+        <li><a href="/snippets">Snippets</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/contact">Contact</a></li>
     </ul>
---
+++ snippets/index.php
@@ -0,0 +1,104 @@
+<?php include('../_inc/header.php'); ?>
+<?php include('../_inc/icons.php'); ?>
+
+<style><?php include($_SERVER["DOCUMENT_ROOT"]."/assets/css/gridder.css"); ?></style>
+
+<div class="">
+
+    <header role="banner" class="snippets">
+        <?php include('../_inc/navigation.php'); ?>
+        <div class="central center">
+            <h1>Snippets</h1>
+            <br><br>
+        </div>
+    </header>
+
+    <br><br>
+
+    <main class="container" style="font-size:16px">
+        <div class="row">
+            <div class="tiny-12 small-6 medium-4">
+                <a class="card" href="snippets/tables">
+                    <div class="card-header">
+                        <h2>Responsive Tables</h2>
+                    </div>
+                    <div class="card-body">
+                        <svg id="table" width="100%" height="auto" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" viewBox="0 0 394 346" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <g stroke="#31315D" stroke-width="3">
+                                <path id="cell-0" d="M132.5,1.50002236 L132.5,50.5 L2.47143119,50.5 L1.69572794,10.1634313 C1.65059763,7.81665504 2.56095883,5.67375519 4.06929668,4.10626684 C5.57467006,2.54185921 7.67567189,1.55060348 10.0168911,1.50184838 L10.0168911,1.50184838 L132.5,1.50002236 Z" fill-opacity="0.15" fill="#31315D"></path>
+                                <rect id="cell-1" fill-opacity="0.15" fill="#31315D" x="132.5" y="1.5" width="130" height="49"></rect>
+                                <path id="cell-2" d="M384,1.5 C386.34721,1.5 388.47221,2.45139491 390.010408,3.98959236 C391.548605,5.52778981 392.5,7.65278981 392.5,10 L392.5,10 L392.5,50.5 L262.5,50.5 L262.5,1.5 Z" fill-opacity="0.15" fill="#31315D"></path>
+                                
+                                <rect id="cell-5" x="2.5" y="50.5" width="130" height="49"></rect>
+                                <rect id="cell-4" x="132.5" y="50.5" width="130" height="49"></rect>
+                                <rect id="cell-3" x="262.5" y="50.5" width="130" height="49"></rect>
+
+                                <rect id="cell-8" x="2.5" y="99.5" width="130" height="49"></rect>
+                                <rect id="cell-7" x="132.5" y="99.5" width="130" height="49"></rect>
+                                <rect id="cell-6" x="262.5" y="99.5" width="130" height="49"></rect>
+
+                                <rect id="cell-11" x="2.5" y="148.5" width="130" height="49"></rect>
+                                <rect id="cell-10" x="132.5" y="148.5" width="130" height="49"></rect>
+                                <rect id="cell-9" x="262.5" y="148.5" width="130" height="49"></rect>
+
+                                <rect id="cell-14" x="2.5" y="197.5" width="130" height="49"></rect>
+                                <rect id="cell-13" x="132.5" y="197.5" width="130" height="49"></rect>
+                                <rect id="cell-12" x="262.5" y="197.5" width="130" height="49"></rect>
+
+                                <rect id="cell-17" x="2.5" y="246.5" width="130" height="49"></rect>
+                                <rect id="cell-16" x="132.5" y="246.5" width="130" height="49"></rect>
+                                <rect id="cell-15" x="262.5" y="246.5" width="130" height="49"></rect>
+
+                                <path id="cell-20" d="M132.5,295.5 L132.5,344.5 L11,344.5 C8.65278981,344.5 6.52778981,343.548605 4.98959236,342.010408 C3.45139491,340.47221 2.5,338.34721 2.5,336 L2.5,336 L2.5,295.5 L132.5,295.5 Z"></path>
+                                <rect id="cell-19" x="132.5" y="295.5" width="130" height="49"></rect>
+                                <path id="cell-18" d="M392.5,295.5 L392.5,336 C392.5,338.34721 391.548605,340.47221 390.010408,342.010408 C388.47221,343.548605 386.34721,344.5 384,344.5 L384,344.5 L262.5,344.5 L262.5,295.5 L392.5,295.5 Z"></path>
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="card-footer"></div>
+                </a>
+            </div>
+
+            <div class="tiny-12 small-6 medium-4">
+                <a class="card" href="snippets/checkboxes">
+                    <div class="card-header">
+                        <h2>Checkbox Toggle</h2>
+                    </div>
+                    <div class="card-body">
+                        <svg width="25%" height="auto" viewBox="0 0 94 209" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M13.5 6.5H74C84.2173 6.5 92.5 14.7827 92.5 25C92.5 35.2173 84.2173 43.5 74 43.5H13.5V6.5Z" fill="#F5F5F5" stroke="#31315D" stroke-width="3"/>
+                            <circle cx="25" cy="25" r="23.5" fill="#D7D7DE" stroke="#31315D" stroke-width="3"/>
+                            <path d="M13.5 165.5H74C84.2173 165.5 92.5 173.783 92.5 184C92.5 194.217 84.2173 202.5 74 202.5H13.5V165.5Z" fill="#D7D7DE" stroke="#31315D" stroke-width="3"/>
+                            <circle cx="25" cy="184" r="23.5" fill="#D7D7DE" stroke="#31315D" stroke-width="3"/>
+                            <path d="M20 86.5H74C84.2173 86.5 92.5 94.7827 92.5 105C92.5 115.217 84.2173 123.5 74 123.5H20C9.78273 123.5 1.5 115.217 1.5 105C1.5 94.7827 9.78273 86.5 20 86.5Z" fill="#D7D7DE" stroke="#31315D" stroke-width="3"/>
+                            <circle cx="69" cy="105" r="23.5" fill="#31315D" stroke="#31315D" stroke-width="3"/>
+                        </svg>
+                    </div>
+                    <div class="card-footer"></div>
+                </a>
+            </div>
+
+            <!-- <div class="tiny-12 small-6 medium-4">
+                <a class="card" href="snippets/radio-buttons">
+                    <div class="card-header">
+                        <h2>Radio Buttons</h2>
+                    </div>
+                    <div class="card-body">
+                        <svg width="70%" height="auto" viewBox="0 0 244 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="25" cy="25" r="23.5" fill="#D7D7DE" stroke="#31315D" stroke-width="3"/>
+                            <circle cx="122" cy="25" r="23.5" fill="#D7D7DE" stroke="#31315D" stroke-width="3"/>
+                            <circle cx="122" cy="25" r="13" fill="#31315D"/>
+                            <circle cx="219" cy="25" r="23.5" fill="#D7D7DE" stroke="#31315D" stroke-width="3"/>
+                            <circle cx="219" cy="25" r="13" fill="#AAAAAF"/>
+                        </svg>
+                    </div>
+                    <div class="card-footer"></div>
+                </a>
+            </div> -->
+            
+        </div>
+    </main>
+
+    <?php include('../_inc/footer.php'); ?>
+
+</div>
