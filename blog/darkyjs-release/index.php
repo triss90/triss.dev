6227ef497d41c1b7b868263d205a7a31c99b6c76
Tristan White <oliver.tristan@gmail.com>
2023-07-28 16:21:13 +0200

    Update article with links

--- blog/darkyjs-release/index.php
+++ blog/darkyjs-release/index.php
@@ -14,19 +14,19 @@ img {
 </style>
 <h1>Darky.js Release</h1>
 <p class='timestamp'><time datetime='2023-07-28'>2023-07-28</time></p><hr>
-<p>After a lot of hard work and dedication, I'm pleased to present a new JavaScript library that enables dark mode on any website without any hassle or need to rework the existing codebase.</p>
-<p><strong>Darky.js</strong> is a simple yet powerful tool that gives developers the capability to switch to a dark theme with just a few lines of code. It offers various customization options, making it adaptable to your design needs. Darky.js has been created with user experience in mind, making it a breeze to switch between light and dark themes.</p>
+<p>After a lot of hard work and dedication, I'm pleased to present <a href="https://darky.app">Darky.js</a>, a new JavaScript library that enables dark mode on any website without any hassle or need to rework the existing codebase.</p>
+<p><a href="https://darky.app">Darky.js</a> is a simple yet powerful tool that gives developers the capability to switch to a dark theme with just a few lines of code. It offers various customization options, making it adaptable to your design needs. <a href="https://darky.app">Darky.js</a> has been created with user experience in mind, making it a breeze to switch between light and dark themes.</p>
 <p><img src="https://triss.dev/examples/darky-js-example.gif" alt="darky.js example" /></p>
 <h2>Key features of Darky.js:</h2>
 <ul>
 <li>Easy Integration: It's as simple as embedding a JS file. No need to rewrite or redesign your existing codebase.</li>
 <li>Customizable: The library provides various customization options for the dark mode button and background.</li>
 <li>Save in Cookies: Option to save user's theme preference using cookies.</li>
-<li>Auto Match OS Theme: Darky.js can automatically match the OS's color scheme preference.</li>
+<li>Auto Match OS Theme: <a href="https://darky.app">Darky.js</a> can automatically match the OS's color scheme preference.</li>
 <li>Callbacks: It provides an optional callback function onChange that is triggered when the dark mode is toggled.</li>
 </ul>
-<p>With Darky.js, your website can now be friendlier to users' eyes during night-time browsing or for those who simply prefer darker themes. I encourage you to give it a try and look forward to seeing all the amazing things you will do with it.</p>
-<p>I am eager to see your feedback and how I can improve Darky.js for you. Let's make the web a darker place together! Happy coding! 💻🌙</p>
+<p>With <a href="https://darky.app">Darky.js</a>, your website can now be friendlier to users' eyes during night-time browsing or for those who simply prefer darker themes. I encourage you to give it a try and look forward to seeing all the amazing things you will do with it.</p>
+<p>I am eager to see your feedback and how I can improve <a href="https://darky.app">Darky.js</a> for you. Let's make the web a darker place together! Happy coding! 💻🌙</p>
 <p>/Tristan White</p>
 
 
--- src/_posts/2023-07-28-darkyjs-release.md
+++ src/_posts/2023-07-28-darkyjs-release.md
@@ -19,9 +19,9 @@ img {
 
 <p class='timestamp'><time datetime='%%date%%'>%%date%%</time></p><hr>
 
-After a lot of hard work and dedication, I'm pleased to present a new JavaScript library that enables dark mode on any website without any hassle or need to rework the existing codebase.
+After a lot of hard work and dedication, I'm pleased to present [Darky.js](https://darky.app), a new JavaScript library that enables dark mode on any website without any hassle or need to rework the existing codebase.
 
-**Darky.js** is a simple yet powerful tool that gives developers the capability to switch to a dark theme with just a few lines of code. It offers various customization options, making it adaptable to your design needs. Darky.js has been created with user experience in mind, making it a breeze to switch between light and dark themes.
+[Darky.js](https://darky.app) is a simple yet powerful tool that gives developers the capability to switch to a dark theme with just a few lines of code. It offers various customization options, making it adaptable to your design needs. [Darky.js](https://darky.app) has been created with user experience in mind, making it a breeze to switch between light and dark themes.
 
 ![darky.js example](https://triss.dev/examples/darky-js-example.gif)
 
@@ -30,11 +30,11 @@ After a lot of hard work and dedication, I'm pleased to present a new JavaScript
 * Easy Integration: It's as simple as embedding a JS file. No need to rewrite or redesign your existing codebase.
 * Customizable: The library provides various customization options for the dark mode button and background.
 * Save in Cookies: Option to save user's theme preference using cookies.
-* Auto Match OS Theme: Darky.js can automatically match the OS's color scheme preference.
+* Auto Match OS Theme: [Darky.js](https://darky.app) can automatically match the OS's color scheme preference.
 * Callbacks: It provides an optional callback function onChange that is triggered when the dark mode is toggled.
 
-With Darky.js, your website can now be friendlier to users' eyes during night-time browsing or for those who simply prefer darker themes. I encourage you to give it a try and look forward to seeing all the amazing things you will do with it.
+With [Darky.js](https://darky.app), your website can now be friendlier to users' eyes during night-time browsing or for those who simply prefer darker themes. I encourage you to give it a try and look forward to seeing all the amazing things you will do with it.
 
-I am eager to see your feedback and how I can improve Darky.js for you. Let's make the web a darker place together! Happy coding! 💻🌙
+I am eager to see your feedback and how I can improve [Darky.js](https://darky.app) for you. Let's make the web a darker place together! Happy coding! 💻🌙
 
 /Tristan White
