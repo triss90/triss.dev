61277dbd721fe7ecb89a3d13dc36ecdc5fcb779d
Tristan White <oliver.tristan@gmail.com>
2023-08-09 20:09:04 +0200

    Make carbon tag visible

--- _inc/footer.php
+++ _inc/footer.php
@@ -11,7 +11,7 @@
             <li><a href="https://www.youtube.com/channel/UCxnhnHu17q4Zk4m9-7BL5dw" target="_blank" rel="noopener"><svg alt="" class="icon icon-youtube" width="12px" height="12px"><use xlink:href="#icon-youtube"></use></svg> YouTube</a></li>
             <li><a class="rss-link" href="https://triss.dev/feed/" title="RSS Feed" target="_blank" rel="noopener"><svg alt="" class="icon icon-rss" width="12px" height="12px"><use xlink:href="#icon-rss"></use></svg> RSS Feed</a></li>
         </ul>
-        <!-- <div class="carbon-wrapper">
+        <div class="carbon-wrapper">
             <div class="carbon-inner">
                 <div class="carbon-text">
                     <span class="carbon-tag">0.01g of CO2/view</span>
@@ -19,7 +19,7 @@
                 </div>
                 <p class="carbon-title">Cleaner than 99% of pages tested</p>
             </div>
-        </div> -->
+        </div>
         <a href="#0" onclick="scrollToTop();return false;" class="totop" title="Go to top" aria-label="Go to top">&#8593; Top &#8593;</a>
     </div>
 </footer>
