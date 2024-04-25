d509b6ebfb385f59fab1cc3ccbd6231d58db08ca
Tristan White <oliver.tristan@gmail.com>
2024-04-25 09:55:19 +0200

    Update 404 page

--- 404/index.php
+++ 404/index.php
@@ -1,20 +1,35 @@
-<?php include('../_inc/header.php'); ?>
-<?php include('../_inc/icons.php'); ?>
+<?php
+    $activePage = "404";
+    $description = "404 - Page could not be found";
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="contact">
+<!--
+	What's up, Inspector Gadget? 🕵️‍♂️
+    This page doesn't exist, but you can view the source here: https://github.com/triss90/triss.dev
+-->
 
-    <header class="blog" style="height: 100vh;">
-        <?php include('../_inc/navigation.php'); ?>
-        <div class="central center" style="margin-top: 30vh;">
-            <h1>404: Page not found :(</h1>
-            <p>The page you are looking for does not exist.</p>
-            <br>
-            <p><a href="/" title="Return to the previous page">&laquo; Go back</a></p>
+<head>
+    <?php include '../inc/meta.php'; ?>
+</head>
+
+<body>
+    <?php include '../inc/themes.php'; ?>
+    <?php include '../inc/navigation.php'; ?>
+
+    <header class="hero container">
+        <div class="row">
+            <div class="tiny tiny-center">
+                <h1>404: Page not found :(</h1>
+                <p>The page you are looking for does not exist.</p>
+                <p><a href="/" title="Return to the previous page">&laquo; Go back</a></p>
+            </div>
         </div>
     </header>
 
+    <?php include '../inc/footer.php'; ?>
+    <?php include '../inc/scripts.php'; ?>
+</body>
 
-
-    <?php include('../_inc/footer.php'); ?>
-
-</div>
+</html>
